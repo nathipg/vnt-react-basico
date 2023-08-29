2b09f63e49ef3fb6c95a2a5f3773560c64f205ee
Nathália Pissuti Gomes <nathalia_gomes@apple.com>
2023-08-29 00:44:01 -0300

    Lista tarefas dinâmica

--- vite-project/src/pages/Inicial/Inicial.jsx
+++ vite-project/src/pages/Inicial/Inicial.jsx
@@ -1,17 +1,19 @@
+import { useState } from 'react';
+
 import { FormCriarTarefa, ListaTarefas } from '../../components';
 
 import style from './Inicial.module.css';
 
-const tarefas = [
-  { id: 1, nome: 'Item 1' },
-  { id: 2, nome: 'Item 2' },
-  { id: 3, nome: 'Item 3' },
-];
-
 const Inicial = () => {
+  const [tarefas, setTarefas] = useState([
+    { id: 1, nome: 'Item 1' },
+    { id: 2, nome: 'Item 2' },
+    { id: 3, nome: 'Item 3' },
+  ]);
+
   return (
     <div className={style.Inicial}>
-      <FormCriarTarefa />
+      <FormCriarTarefa tarefas={tarefas} />
       <ListaTarefas tarefas={tarefas} />
     </div>
   );
