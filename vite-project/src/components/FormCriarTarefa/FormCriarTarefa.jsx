a630b60d2c107cf10fc1cdabebd53cbe8a588530
Nathália Pissuti Gomes <nathalia_gomes@apple.com>
2023-08-29 00:26:00 -0300

    Campo texto controlado

--- vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
+++ vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+
 import { Botao, CampoTexto } from '../../components';
 
 import style from './FormCriarTarefa.module.css';
 
 const FormCriarTarefa = () => {
+  const [nomeTarefa, setNomeTarefa] = useState('');
+
+  const onChangeNomeTarefa = (event) => {
+    setNomeTarefa(event.currentTarget.value);
+  };
+
   return (
     <form className={style.FormCriarTarefa}>
-      <CampoTexto />
+      <CampoTexto 
+        value={nomeTarefa} 
+        onChange={onChangeNomeTarefa} 
+      />
       <Botao texto="+" />
     </form>
   );
