36d05ceb3a6cec351fdc0152c8e715fd9a80e7aa
Nathália Pissuti Gomes <nathalia_gomes@apple.com>
2023-09-05 14:38:43 -0300

    useState

--- vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
+++ vite-project/src/components/FormCriarTarefa/FormCriarTarefa.jsx
@@ -1,11 +1,46 @@
+import { useState } from 'react';
+
 import { Botao, CampoTexto } from '../../components';
 
 import style from './FormCriarTarefa.module.css';
 
-const FormCriarTarefa = () => {
+const FormCriarTarefa = (props) => {
+  const [ nomeTarefa, setNomeTarefa ] = useState('');
+
+  const { setTarefas } = props;
+
+  const onChangeNomeTarefa = (event) => {
+    setNomeTarefa(event.currentTarget.value);
+  };
+
+  const adicionarTarefa = (event) => {
+    event.preventDefault();
+
+    if(!nomeTarefa) {
+      return;
+    }
+
+    setTarefas(estadoAtual => {
+      const tarefa = {
+        id: estadoAtual.length + 1,
+        nome: nomeTarefa,
+      };
+
+      return [
+        ...estadoAtual,
+        tarefa,
+      ];
+    });
+
+    setNomeTarefa('');
+  };
+  
   return (
-    <form className={style.FormCriarTarefa}>
-      <CampoTexto />
+    <form className={style.FormCriarTarefa} onSubmit={adicionarTarefa}>
+      <CampoTexto 
+        value={nomeTarefa} 
+        onChange={onChangeNomeTarefa} 
+      />
       <Botao texto="+" />
     </form>
   );
