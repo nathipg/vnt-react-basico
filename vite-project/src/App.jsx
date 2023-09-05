591a383497c7349850b858b614ada704ca9e7744
Nathália Pissuti Gomes <nathalia_gomes@apple.com>
2023-09-05 12:09:32 -0300

    FormCriarTarefa

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,5 @@
 import { Cabecalho, Conteudo, Rodape } from './components';
+import { Inicial } from './pages';
 
 import './App.css';
 
@@ -7,8 +8,7 @@ const App = () => {
     <>
       <Cabecalho nomeUsuario="Joana" />
       <Conteudo>
-        <h1>Titulo</h1>
-        <p>Texto texto texto</p>
+        <Inicial />
       </Conteudo>
       <Rodape criador="Pissuti" />
     </>
--- vite-project/src/components/index.js
+++ vite-project/src/components/index.js
@@ -1,3 +1,6 @@
+export * from './Botao';
 export * from './Cabecalho';
+export * from './CampoTexto';
 export * from './Conteudo';
+export * from './FormCriarTarefa';
 export * from './Rodape';
