d1609399f84b8bedb533075c958d410523b58436
Nathália Pissuti Gomes <nathalia_gomes@apple.com>
2023-09-05 11:05:55 -0300

    2.1 2.2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,14 @@
+import { Cabecalho } from './components';
+
 import './App.css';
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Cabecalho />
+    </>
   )
 };
 
-export default App;
+export { App };
